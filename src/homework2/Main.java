2c4906cc69ee28512958689e2da130c781c6c8cc
SkymanSky <gokera@ogr.iu.edu.tr>
2022-09-23 21:47:23 +0300

    Metodları Anlamak

--- src/homework2/Main.java
+++ src/homework2/Main.java
@@ -3,8 +3,40 @@ package homework2;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Metodları Anlamak
+		sayiBulmaca();
+		sayiBulmaca();
+		sayiBulmaca();
+		sayiBulmaca();
 
+	}
+	
+	public static void sayiBulmaca() {
+		int[] sayilar = new int[] {1,2,5,7,9,0};
+		int aranacak=5;
+		boolean varMi=false;
+
+		for (int sayi:sayilar) {
+			if(sayi==aranacak) {
+				varMi=true;
+				break;
+			}
+		}
+		
+		String mesaj="";
+		if (varMi) {
+			mesaj="Sayı mevcuttur: "+aranacak;
+			mesajVer(mesaj);
+		}else {
+			mesajVer("Sayı mevcut değildir: "+aranacak);
+		}
+		
+		
+	}
+	
+	public static void mesajVer(String mesaj) {
+		System.out.println(mesaj);
+		
 	}
 
 }
