f767475feb85a2d4d1ef077a1772ac900ac2b3cd
SkymanSky <gokera@ogr.iu.edu.tr>
2022-09-24 22:22:22 +0300

    Bir Class Nasıl Çalışır

--- src/homework2/Main.java
+++ src/homework2/Main.java
@@ -3,8 +3,14 @@ package homework2;
 public class Main {
 
 	public static void main(String[] args) {
-		// Class Nedir? - Doğru Anlamak
-		
+		// Bir Class Nasıl Çalışır
+		// referance type
+		CustomerManager customerManager = new CustomerManager();
+		CustomerManager customerManager2 = new CustomerManager();
+		customerManager=customerManager2;
+		customerManager.Add();
+		customerManager.Remove();
+		customerManager.Update();
 		
 		
 		
